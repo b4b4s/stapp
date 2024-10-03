d379e0b4cee4e83c72942d156ac4b5b151e79fe6
b4b4s <obrero@gmail.com>
2024-10-03 12:33:32 +0100

    col

--- home.py
+++ home.py
@@ -18,12 +18,13 @@ col1, col2 = st.columns(2)
 
 # first column
 with col1:
-    st.subheader(":black_nib: Name: Elias Ortiz")
-    st.subheader(":male-technologist: Position: Senior Geophysicist")
-    st.subheader(":earth_americas: Nationality: Mexican :flag-mx:")
-    st.subheader("round_pushpin: Location: Aberdeen")
-    st.subheader("📊 Interests: Geophysics, Multivariate Statistics\
-                  Data Science & Machine Learning, Product Development")
+    st.markdown(":black_nib: **Name**: Elias Ortiz")
+    # st.subheader(":black_nib: Name: Elias Ortiz")
+    # st.subheader(":male-technologist: Position: Senior Geophysicist")
+    # st.subheader(":earth_americas: Nationality: Mexican :flag-mx:")
+    # st.subheader("round_pushpin: Location: Aberdeen")
+    # st.subheader("📊 Interests: Geophysics, Multivariate Statistics\
+    #               Data Science & Machine Learning, Product Development")
 
 with col2:
     st.header("")
